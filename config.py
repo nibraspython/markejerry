acd861bb30a4880dc19dfde265381143218135fd
nibras <brihsan40@gmail.com>
2025-03-15 05:56:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "28152382"))
-API_HASH = os.getenv("API_HASH", "c5a9a284a66ebc8c5ee56f72f18c9b53")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7685121222:AAG_4j0pq-_8IQGz-Aaqs1WKBuJjzzgvaG8")
+class Config:
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    ADMIN = [int(i) for i in os.getenv("ADMIN", "").split() if i.isdigit()]
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL")) if os.getenv("LOG_CHANNEL") else None
+    WEB_SUPPORT = bool(os.getenv("WEB_SUPPORT", true))
+    MAX_CONCURRENT_TRANSMISSIONS = 5
+    BOT_UPTIME = "Bot is running..."
